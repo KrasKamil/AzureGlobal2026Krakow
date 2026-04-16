c8c2e12697aecabdca0e832db857221b1c61634b
KrasKamil <96253806+KrasKamil@users.noreply.github.com>
2026-04-16 21:17:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,8 +82,8 @@ module "app_service" {
     location = "polandcentral" 
   }
   app_service_plan_id = module.service_plan.app_service_plan.id
-  identity_id        = module.managed_identity.managed_identity_id.id
-  identity_client_id  = module.managed_identity.managed_identity_client_id.id 
+  identity_id        = module.managed_identity.managed_identity_id
+  identity_client_id  = module.managed_identity.managed_identity_client_id 
   app_settings = {
     "INSTRUMENTATION_KEY" = module.application_insights.instrumentation_key
   }
