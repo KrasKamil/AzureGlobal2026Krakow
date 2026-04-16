0518c20c064024cfa4e9b26913bf31797365d0ce
KrasKamil <96253806+KrasKamil@users.noreply.github.com>
2026-04-16 21:03:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "app_service" {
   identity_id        = module.managed_identity.managed_identity_id.id
   identity_client_id  = module.managed_identity.managed_identity_client_id.id 
   app_settings = {
-    "INSTRUMENTATION_KEY" = module.application_insights.application_insights.instrumentation_key
+    "INSTRUMENTATION_KEY" = module.application_insights.instrumentation_key.id
     "DB_SERVER"           = module.mssql_server.mssql_server.fully_qualified_domain_name
   }
 }
