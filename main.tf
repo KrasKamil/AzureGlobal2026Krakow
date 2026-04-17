3e349586172457160c936f6bff19b94499bfacac
KrasKamil <96253806+KrasKamil@users.noreply.github.com>
2026-04-17 08:48:05 +0200

    Remove mssql_server module from main.tf
    Removed mssql_server module and its associated configurations.

--- main.tf
+++ main.tf
@@ -56,15 +56,19 @@ tags = {
   }
 }
 module "mssql_server" {
-  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
+   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
+  
+  sql_server_name    = "sql-kamil-user9" 
+  sql_server_admin   = "sqladmin"
+  sql_server_password = "mojeSuperHaslo123!" 
+  
   resource_group = {
     name     = "rg-user9"
     location = "polandcentral" 
   }
-  sql_server_admin = "KamilAdmin"
-   sql_server_name    = "mysql-kamil-database-unique" 
-  sql_server_version = "12.0"
+  database_name = "webappdb" 
 }
+
 module "application_insights" {
   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=application_insights/v1.0.0"
   application_insights_name = "smartappKamilhouse"
@@ -100,18 +104,3 @@ module "container_registry" {
     location = "polandcentral" 
   }
 }
-module "mssql_server" {
-   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
-  
-  sql_server_name    = "sql-kamil-user9" 
-  sql_server_admin   = "sqladmin"
-  sql_server_password = "mojeSuperHaslo123!" 
-  
-  resource_group = {
-    name     = "rg-user9"
-    location = "polandcentral" 
-  }
-  database_name = "webappdb" 
-}
-
-
